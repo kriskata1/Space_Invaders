fd3cefb9c750db6dd0b3622c5f2c6602d6300112
OzanAdil <ozanadil04@gmail.com>
2025-05-26 21:27:49 +0300

    put back bullets (accidental deletion)

---
+++ bullet.cpp
@@ -0,0 +1,42 @@
+#include "bullet.h"
+#include "player.h"
+
+extern void draw_char(char ch, int y, int x, COLORS foreground, COLORS background);
+
+Bullet::Bullet():GameObject(0,0,' ',BLACK), direction(-1){}
+Bullet::Bullet(int x, int y, char symbol, COLORS color, int direction) : GameObject(x, y, (direction == -1) ? '|' : '*', WHITE), direction(direction) {} //default
+Bullet::Bullet(const Bullet& other) : GameObject(other), direction(other.direction) {} //copy
+Bullet::Bullet(Bullet&& other) noexcept : GameObject(std::move(other)), direction(other.direction) {}
+
+Bullet &Bullet::operator=(const Bullet& other) {
+    if (this != &other) {
+        GameObject::operator=(other);
+        direction = other.direction;
+    }
+
+    return *this;
+}
+
+Bullet &Bullet::operator=(Bullet &&other) noexcept {
+    if (this != &other) {
+        GameObject::operator=(std::move(other));
+        direction = other.direction;
+    }
+
+    return *this;
+}
+
+
+void Bullet::update() {
+    setY(getY() + direction);
+}
+
+//out of bounds?
+
+int Bullet::getDirection()const {
+    return direction;
+}
+
+void Bullet::render() {
+    draw_char(getSymbol(), getY(), getX(), getColor(), BLACK);
+}
---
+++ bullet.h
@@ -0,0 +1,25 @@
+#include "gameObject.h"
+
+class Bullet : public GameObject {
+public:
+    Bullet();
+    Bullet(int x, int y, char symbol, COLORS color, int direction);
+    Bullet(const Bullet &other); //copy
+    Bullet(Bullet &&other) noexcept ; //move
+
+    Bullet& operator=(const Bullet& other);
+    Bullet& operator=(Bullet&& other) noexcept;
+
+    //metodi
+    void update() override;
+    void render() override;
+
+    int getDirection() const;
+private:
+    int direction;
+};
+
+#ifndef BULLET_H
+#define BULLET_H
+
+#endif //BULLET_H
